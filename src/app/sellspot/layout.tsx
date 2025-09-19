1e32ee52982e1cebe3ca00532dfbcc4c7485b054
Gulpanjul <gulpa.andhikac@gmail.com>
2025-09-19 13:57:02 +0700

    refactor: change sellspot location

---
+++ src/app/sellspot/layout.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Poppins } from "next/font/google";
+import { useEffect } from "react";
+import LandingPageLayoutFooter from "~/app/sellspot/components/layouts/LandingPageLayout/LandingPageLayoutFooter";
+import LandingPageLayoutNavbar from "~/app/sellspot/components/layouts/LandingPageLayout/LandingPageLayoutNavbar";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-poppins",
+});
+
+interface PropTypes {
+    children: React.ReactNode;
+}
+
+const SellSpotLayout = (props: PropTypes) => {
+    const { children } = props;
+    const { setTheme } = useTheme();
+
+    useEffect(() => {
+        setTheme("sellspot");
+    }, [setTheme]);
+
+    return (
+        <main
+            className={`${poppins.variable} theme-sellspot bg-dark text-light`}
+        >
+            <LandingPageLayoutNavbar />
+            {children}
+            <LandingPageLayoutFooter />
+        </main>
+    );
+};
+
+export default SellSpotLayout;
---
+++ src/app/sellspot/page.tsx
@@ -0,0 +1,17 @@
+import FaqSection from "./components/views/Home/FaqSection";
+import FeatureSection from "./components/views/Home/FeatureSection";
+import HeroSection from "./components/views/Home/HeroSection";
+import MottoSection from "./components/views/Home/MottoSection";
+
+const sellSpotPage = () => {
+    return (
+        <>
+            <HeroSection />
+            <MottoSection />
+            <FeatureSection />
+            <FaqSection />
+        </>
+    );
+};
+
+export default sellSpotPage;
