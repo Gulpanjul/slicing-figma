516648a747351dd985090223a22abaf616ad2674
gulpanjul <gulpa.andhikac@gmail.com>
2025-09-24 16:05:11 +0700

    refactor: banner section code structure

--- src/components/views/Videfly/Main/BannerSection/Banner.constant.tsx
+++ src/components/views/Videfly/Main/BannerSection/Banner.constant.tsx
@@ -1,17 +1,26 @@
+import Video from "~/components/assets/Videfly/video.svg";
+import Textbox from "~/components/assets/Videfly/textbox.svg";
+import UserTalk from "~/components/assets/Videfly/user-talk.svg";
+import UserStar from "~/components/assets/Videfly/user-star.svg";
+
 const ACTION_ITEMS = [
     {
+        icon: <Video />,
         label: "URL to video",
         href: "#",
     },
     {
+        icon: <Textbox />,
         label: "Text to Video",
         href: "#",
     },
     {
+        icon: <UserTalk />,
         label: "Speech to Video",
         href: "#",
     },
     {
+        icon: <UserStar />,
         label: "AI Avatar",
         href: "#",
     },
--- src/components/views/Videfly/Main/BannerSection/BannerSection.tsx
+++ src/components/views/Videfly/Main/BannerSection/BannerSection.tsx
@@ -1,20 +1,28 @@
 import { ACTION_ITEMS } from "./Banner.constant";
 import Banner from "~/components/assets/Videfly/banner.svg";
 
+const sectionClasses = "flex items-center w-full justify-center";
+const wrapperClasses = "relative flex items-center justify-center";
+const bannerClasses = "w-full h-auto";
+const titleClasses =
+    "absolute text-[2rem] font-extrabold text-white font-[Nunito]";
+const actionWrapperClasses =
+    "absolute bottom-0 translate-y-1/2 bg-white py-2 px-2 rounded-2xl border border-[#ECECEC] flex";
+const actionButtonClasses =
+    "py-2.5 px-8 hover:bg-[#ECECEC] rounded-2xl transition flex gap-3 items-center font-semibold";
+
 const BannerSection = () => {
     return (
-        <section className="flex items-center w-full justify-center">
-            <div className="relative flex items-center justify-center">
-                <Banner height="11.25rem" className="w-full h-auto" />
-                <p className="absolute text-[2rem] font-extrabold text-[#FFFFFF] font-[Nunito]">
-                    Mulai kreasi baru hari ini!
-                </p>
-                <div className="absolute bottom-0 translate-y-1/2 bg-[#FFFFFF] py-2 px-2 rounded-2xl border-[0.063rem] border-[#ECECEC]">
+        <section className={sectionClasses}>
+            <div className={wrapperClasses}>
+                <Banner height="11.25rem" className={bannerClasses} />
+
+                <p className={titleClasses}>Mulai kreasi baru hari ini!</p>
+
+                <div className={actionWrapperClasses}>
                     {ACTION_ITEMS.map((item, index) => (
-                        <button
-                            key={index}
-                            className="py-2.5 px-8 hover:bg-[#ECECEC] rounded-2xl transition"
-                        >
+                        <button key={index} className={actionButtonClasses}>
+                            {item.icon}
                             {item.label}
                         </button>
                     ))}
