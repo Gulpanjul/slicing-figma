2231c20c1db22de6c217515e1ad0b159161b09aa
Gulpanjul <gulpa.andhikac@gmail.com>
2025-09-30 01:12:00 +0700

    refactor: update services section into clean code

--- src/components/views/Lidia/Main/ServicesSection/ServicesSection.tsx
+++ src/components/views/Lidia/Main/ServicesSection/ServicesSection.tsx
@@ -1,63 +1,77 @@
 import Image from "next/image";
 import Title from "~/components/ui/Lidia/Title";
 
+const style = {
+  article: "grid gap-[125px] mb-[140px]",
+
+  firstLayout: "grid-cols-[600px_411px]",
+  secondLayout: "grid-cols-[419px_1fr] gap-[181px]",
+  imageWrapper:
+    "relative h-[399px] w-[600px] shadow-[0_30px_60px_rgba(71,74,87,0.25)] rounded-[10px]",
+  image: "object-cover rounded-[10px]",
+  textBlock: "w-full max-w-[411px] tracking-[0.02em] text-[#5E6282]",
+  heading: "mb-10 text-[32px] font-medium leading-snug",
+  highlight: "text-[#4475F2]",
+  paragraph: "mb-[15px]",
+};
+
 const ServicesSection = () => {
   return (
     <section>
       <Title heading="Services" desc="🚀• The Services for You" />
-      <article className="grid grid-cols-[600px_411px] justify-between">
-        <div className="relative -left-[46px] h-[399px] w-[600px] justify-self-start">
+
+      <article className={`${style.article} ${style.firstLayout}`}>
+        <div className={`${style.imageWrapper} justify-self-start`}>
           <Image
             src="/images/Lidia/Service.png"
-            alt=""
+            alt="Service Image"
             fill
-            className="object-cover"
+            className={style.image}
           />
         </div>
-        <div className="mr-[44px]">
-          <h3 className="mb-10 text-[32px] font-medium">
-            <span className="text-[#4475F2]">Rent</span> your favorite book
-            fairly easy on <span className="text-[#4475F2]">Lidia</span>!
+
+        <div className={style.textBlock}>
+          <h3 className={style.heading}>
+            <span className={style.highlight}>Rent</span> your favorite book
+            fairly easy on <span className={style.highlight}>Lidia</span>!
           </h3>
-          <div className="tracking-[2%] text-[#5E6282]">
-            <p className="mb-[15px]">
-              Viewing, rent, and organize your favorite books has never been
-              easier. An integrated digital library rent that’s simple to use,
-              Lidia lets you spend less time managing your work and more time
-              actually doing it!
-            </p>
-            <p>
-              Effortless rentals, personalized shelves—Lidia transforms book
-              management, enhancing your reading experience~
-            </p>
-          </div>
+          <p className={style.paragraph}>
+            Viewing, rent, and organize your favorite books has never been
+            easier. An integrated digital library rent that’s simple to use,
+            Lidia lets you spend less time managing your work and more time
+            actually doing it!
+          </p>
+          <p>
+            Effortless rentals, personalized shelves—Lidia transforms book
+            management, enhancing your reading experience~
+          </p>
         </div>
       </article>
-      <article className="grid grid-cols-[419px_1fr] justify-between">
-        <div>
-          <h3 className="mb-10 text-[32px] font-medium">
-            Quick Book Rentals: <span className="text-[#4475F2]">Dive</span>{" "}
-            into <span className="text-[#4475F2]">Reading</span> Instantly
+
+      <article className={`${style.article} ${style.secondLayout}`}>
+        <div className={style.textBlock}>
+          <h3 className={style.heading}>
+            Quick Book Rentals: <span className={style.highlight}>Dive</span>{" "}
+            into <span className={style.highlight}>Reading</span> Instantly
           </h3>
-          <div className="tracking-[2%] text-[#5E6282]">
-            <p className="mb-[15px]">
-              Discover instant literary delight. Access a vast library, borrow
-              your favorite reads, and dive into captivating stories within
-              minutes. Reading made quick and easy, just a click away!
-            </p>
-            <p>
-              Unlock a world of stories effortlessly. Browse genres, choose,
-              rent in minutes. Seamlessly manage your reading adventures with
-              our intuitive platform~
-            </p>
-          </div>
+          <p className={style.paragraph}>
+            Discover instant literary delight. Access a vast library, borrow
+            your favorite reads, and dive into captivating stories within
+            minutes. Reading made quick and easy, just a click away!
+          </p>
+          <p>
+            Unlock a world of stories effortlessly. Browse genres, choose, rent
+            in minutes. Seamlessly manage your reading adventures with our
+            intuitive platform~
+          </p>
         </div>
-        <div className="relative -right-[50px] h-[399px] w-[600px] items-center justify-self-end">
+
+        <div className={`${style.imageWrapper} justify-self-end`}>
           <Image
             src="/images/Lidia/Reading.png"
-            alt=""
+            alt="Reading Image"
             fill
-            className="object-cover"
+            className={style.image}
           />
         </div>
       </article>
